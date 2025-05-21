0e8c10e56ee050108b49e21b938cb5ecee019e76
santhosh2k25 <santhosh@octogenie.com>
2025-05-21 18:12:33 +0530

    build logs

--- b2b-code/vite.config.ts
+++ b2b-code/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
@@ -15,6 +16,9 @@ export default ({ mode }: { mode: string }) => {
     build: {
       chunkSizeWarningLimit: 1500,
       rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, 'index.html'),
+        },
         output: {
           manualChunks: {
             'react-vendor': ['react', 'react-dom', 'react-router-dom'],
